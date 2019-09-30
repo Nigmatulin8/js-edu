01d21d5fdb09d8e87da1bc410f2da961b13a84d7
Nigmatulin8 <rustam280897@gmail.com>
2019-09-30 15:52:40 +0400

    Solution

---
+++ src/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param preferences - target student focus
+ * @param knowsProgramming - if student can do programming and know basics
+ * @param config - private student ability to perform for different focus modes
+ * @returns number of weeks needed for finish education
+ */
+module.exports = function getTimeForEducation(
+    focus = 'family', 
+    knowsProgramming = true,
+    config = { family: 4 }
+    ) {
+
+    return Math.ceil((!knowsProgramming ? 1300 : 800) / config[focus]);
+  };
+  
